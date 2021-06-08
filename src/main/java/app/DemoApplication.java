7a49e6fc59a15bc5d0dfe0df1988c83b8f1beff2
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2021-06-08 11:49:58 -0300

    implementação do swagger
    localhost:8080/swagger-ui

--- src/main/java/app/DemoApplication.java
+++ src/main/java/app/DemoApplication.java
@@ -6,14 +6,16 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+
 @SpringBootApplication
 @EnableJpaRepositories({"domain"})
 @EntityScan({"domain"})
 @ComponentScan({"app"})
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
